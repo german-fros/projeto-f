16948b9dfed10ef1d0fe29a526bc3e1fb6c52b77
German Fros <german.fros551@gmail.com>
2023-04-18 15:49:59 -0300

    Finalizado (espero)

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function abrir() {
     var msg = document.getElementById('msg')
+    var sorry = document.querySelector('#sorry')
+    var button = document.querySelector('#button')
+    button.style.display = 'none'
+    sorry.style.display = 'block'
     msg.style.opacity = '1'
     msg.style.transition = 'opacity 1s ease-out'
 }
